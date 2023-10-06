d553dfc1d67a2073387f1b8ccc24840edfca4e64
sanctum32 <andriuspel@gmail.com>
2023-10-07 01:56:20 +0300

    More improvements for quest "Last Chance at Humanity"(14375)

--- sql/updates/WIP/gilneas.sql
+++ sql/updates/WIP/gilneas.sql
@@ -226,10 +226,13 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 -- Last Chance at Humanity
 -- -------------------------
 
--- TODO: need to confirm
--- UPDATE `quest_template` SET `Flags` = `Flags`|65536 WHERE `ID` = 14375;
+-- Quest should instantly point to reward window
+UPDATE quest_template set flags=flags|0x00040000 WHERE id=14375;
 
-DELETE FROM `smart_scripts` WHERE `entryorguid`=3633000 AND `source_type`=9 AND `id`=0 AND `link`=0;
+-- Quest giver flag should be added in timed action
+UPDATE creature_template SET npcflag=npcflag &~ 2 WHERE entry=36332;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=3633000 AND `source_type`=9;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `event_param5`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
 (3633000, 9, 0, 0, 0, 0, 100, 0, 1200, 1200, 0, 0, 0, 59, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'Set Walk'),
 (3633000, 9, 1, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 69, 1, 0, 0, 0, 0, 0, 8, 0, 0, 0, -1840.08, 2293.04, 42.53, 0, 'Move to Pos'),
